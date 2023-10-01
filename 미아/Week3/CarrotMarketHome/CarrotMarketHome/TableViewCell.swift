d1628c3b92b8a3aaf7231a952f59386e5b856dbe
IMseahkim <sookseah@sookmyung.ac.kr>
2023-10-02 00:29:13 +0900

    .gitignore에 .DS_Store 추가

---
+++ 미아/Week3/CarrotMarketHome/CarrotMarketHome/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  CarrotMarketHome
+//
+//  Created by 김세아 on 2023/09/29.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 미아/Week3/CarrotMarketHome/CarrotMarketHome/UIViewExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewExtensions.swift
+//  CarrotMarketHome
+//
+//  Created by 김세아 on 10/1/23.
+//
+
+import Foundation
